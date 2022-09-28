3b0dab77103de9e90d054e063bf2e0493eb36d26
Muhammed Asharaf K V <51042907+winash1618@users.noreply.github.com>
2022-09-28 13:42:23 +0000

    Data Types

--- 3.Common Programming Concepts/variables/src/main.rs
+++ 3.Common Programming Concepts/variables/src/main.rs
@@ -1,12 +1,22 @@
+use std::io;
+
 fn main() {
-    let x = 5;
+    let a = [1, 2, 3, 4, 5];
 
-    let x = x + 1;
+    println!("Please enter an array index.");
 
-    {
-        let x = x * 2;
-        println!("The value of x in the inner scope is: {x}");
-    }
+    let mut index = String::new();
 
-    println!("The value of x is: {x}");
+    io::stdin()
+        .read_line(&mut index)
+        .expect("Failed to read line");
+
+    let index: usize = index
+        .trim()
+        .parse()
+        .expect("Index entered was not a number");
+
+    let element = a[index];
+
+    println!("The value of the element at index {index} is: {element}");
 }
