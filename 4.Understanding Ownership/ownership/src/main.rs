0a5644c84ce6020cc8a1a70320d9f81b87ae5356
Muhammed Asharaf K V <51042907+winash1618@users.noreply.github.com>
2022-09-30 16:10:33 +0000

    String slices first word and second word

--- 4.Understanding Ownership/ownership/src/main.rs
+++ 4.Understanding Ownership/ownership/src/main.rs
@@ -1,13 +1,37 @@
-fn main() {
-    let s1 = String::from("hello");
-
-    let (s2, len) = calculate_length(s1);
-
-    println!("The length of '{}' is {}.", s2, len);
+fn second_word(s: &String) -> &str {
+	let bytes = s.as_bytes();
+	let mut k = 0;
+	let mut j = 0;
+	for (i, &item) in bytes.iter().enumerate() {
+		if item == b' ' && k == 0{
+			k = 1;
+			j = i;
+		}
+		else if item == b' ' && k == 1 {
+			return &s[j + 1..i];
+		}
+	}
+	if k == 1{
+		return &s[j + 1..];}
+	&s[..]
 }
 
-fn calculate_length(s: String) -> (String, usize) {
-    let length = s.len(); // len() returns the length of a String
+fn first_word(s: &String) -> &str {
+	let bytes = s.as_bytes();
 
-    (s, length)
+	for (i, &item) in bytes.iter().enumerate() {
+		if item == b' ' {
+			return &s[0..i];
+		}
+	}
+	&s[..]
+}
+fn main() {
+    let s = String::from("hello world");
+	let first = first_word(&s);
+    let word = second_word(&s); // word will get the value 5
+	println!("{first}");
+	println!("{word}");
+    // word still has the value 5 here, but there's no more string that
+    // we could meaningfully use the value 5 with. word is now totally invalid!
 }
