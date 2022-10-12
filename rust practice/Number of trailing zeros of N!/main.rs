402f1c0b7d74ca893c95a15291ba5ac10030d21f
Muhammed Asharaf K V <51042907+winash1618@users.noreply.github.com>
2022-10-12 09:51:10 +0000

    Number of trailing zeros of N!

--- rust practice/Number of trailing zeros of N!/main.rs
+++ rust practice/Number of trailing zeros of N!/main.rs
@@ -1,28 +1,56 @@
-// timed out solution. Not good.
+// https://www.purplemath.com/modules/factzero.htm - Solution idea got from here.
+
 fn zeros(n: u64) -> u64 {
     let mut num = 5;
-    let mut k = 0;
-    while num <= n
+    let mut count = 0;
+    let cpy = n;
+    while n / num > 0
     {
-        let j = num;
-        if num % 10 == 0
-        {
-            while num % 10 == 0 && num > 0
-            {
-                k += 1;
-                num /= 10;
-            }
-        }
-        if num % 5 == 0
-        {
-            while num % 5 == 0 && num > 0
-            {
-                k += 1;
-                num /= 5;
-            }
-        }
-        num = j;
-        num += 5
+        count += cpy / num;
+        num *= 5;
     }
-    return k;
+    return count;
 }
+
+
+/*************************************************************************/
+/*************************** Best Solution *******************************/
+/*************************************************************************/
+
+fn zeros(n: u64) -> u64 {
+//     println!("N -> {}", n);
+	if n == 0 { 0 } 
+	else { n / 5 + zeros(n / 5) }
+}
+
+
+// timed out solution. Not good.
+
+
+// fn zeros(n: u64) -> u64 {
+//     let mut num = 5;
+//     let mut k = 0;
+//     while num <= n
+//     {
+//         let j = num;
+//         if num % 10 == 0
+//         {
+//             while num % 10 == 0 && num > 0
+//             {
+//                 k += 1;
+//                 num /= 10;
+//             }
+//         }
+//         if num % 5 == 0
+//         {
+//             while num % 5 == 0 && num > 0
+//             {
+//                 k += 1;
+//                 num /= 5;
+//             }
+//         }
+//         num = j;
+//         num += 5
+//     }
+//     return k;
+// }
