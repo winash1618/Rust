5333519b7b821323c3621827c37b4ba9e61304b9
Muhammed Asharaf K V <51042907+winash1618@users.noreply.github.com>
2022-10-13 12:23:58 +0000

    name changes and final updated on Square into Squares Protect trees

--- rust practice/Square into Squares Protect trees!/main.rs
+++ rust practice/Square into Squares Protect trees!/main.rs
@@ -1,11 +1,19 @@
 // Working but timed out
 fn decompose(n: i64) -> Option<Vec<i64>> {
+	if n < 4
+	{
+		return None;
+	}
     let mut vec : Vec<i64> = Vec::new();
     vec.push(n - 1);
     while vec.iter().map(|x| x.pow(2)).sum::<i64>() < n * n
     {
         let sum = n * n - vec.iter().map(|x| x.pow(2)).sum::<i64>();
         let sum : i64 = (sum as f64).sqrt() as i64;
+		if sum * sum >= vec.iter().map(|x| x.pow(2)).sum::<i64>()
+		{
+			break ;
+		}
 		if vec[vec.len() - 1] > sum
 		{
 			vec.push(sum);
@@ -15,12 +23,8 @@ fn decompose(n: i64) -> Option<Vec<i64>> {
 			if vec.len() > 1
 			{
 				let tmp = vec[vec.len() - 2];
-				println!("ls {} {}", vec[vec.len() - 1], vec[vec.len() - 2]);
 				vec.pop();
 				vec.pop();
-				// let sum = n * n - vec.iter().map(|x| x.pow(2)).sum::<i64>();
-				// let sum : i64 = (sum as f64).sqrt() as i64;
-				// println!("sum {} and length {}", sum, vec.len());
 				vec.push(tmp - 1);
 			}
 		}
@@ -31,7 +35,6 @@ fn decompose(n: i64) -> Option<Vec<i64>> {
         let mut tmp : Vec<i64> = Vec::new();
         while vec.len() != 0
         {
-			println!("{} {}", vec[vec.len() - 1], vec.len() - 1);
             tmp.push(vec[vec.len() - 1]);
             vec.pop();
         }
@@ -42,10 +45,30 @@ fn decompose(n: i64) -> Option<Vec<i64>> {
         None
     }
 }
-fn main()
-{
-	decompose(50);
-}
+// fn main()
+// {
+// 	decompose(5);
+// }
+
+/*************************************************************************/
+/*************************** Best Solution *******************************/
+/*************************************************************************/
+// fn decompose(n: i64) -> Option<Vec<i64>> {
+//     partition(n * n, n)
+// }
+
+// fn partition(sqrt: i64, n: i64) -> Option<Vec<i64>> {
+//     if sqrt < 0 { return None; }
+//     if sqrt == 0 { return Some(Vec::new()); }
+
+//     for i in (0..n).rev() {
+//         let part = partition(sqrt - i * i, i);
+//         if part != None { return Some([part.unwrap(), vec![i]].concat()); }
+//     }
+
+//     None
+// }
+
 // Not Working
 // fn decompose(n: i64) -> Option<Vec<i64>> {
 //     let mut vec : Vec<i64> = Vec::new();
