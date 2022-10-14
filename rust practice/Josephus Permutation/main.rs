7159f95003cd7096d2b52426f78106fdfa8273ea
Muhammed Asharaf K V <51042907+winash1618@users.noreply.github.com>
2022-10-14 13:26:16 +0000

    final working version with debug part

--- rust practice/Josephus Permutation/main.rs
+++ rust practice/Josephus Permutation/main.rs
@@ -1,19 +1,31 @@
-fn josephus_recursion<T:Clone+Copy>(xs:Vec<T>, k:usize, l:usize) -> Vec<T> {
+fn josephus_recursion<T:Clone+Copy + std::fmt::Debug>(xs:Vec<T>, k:usize, loc:usize) -> Vec<T> {
     let mut vec = Vec::new();
 	let mut rem = k;
+    let mut loc = loc;
+    let mut xs = xs;
+    
     if xs.len() > 0
 	{
-		if (xs.len() < k)
+		if xs.len() < k
 		{
+			println!("1loc {} rem {} len {}", loc, rem, xs.len());
 			rem = k % xs.len();
 		}
-		if loc + rem >= xs.len()
+		if loc == 0 && rem == 0
 		{
-			loc += rem - xs.len();
+			println!("2loc {} rem {} len {}", loc, rem, xs.len());
+			loc = xs.len() - 1;
+		}
+		else if (loc + rem) > xs.len()
+		{
+			println!("3loc {} rem {} len {}", loc, rem, xs.len());
+			loc = (loc + rem) - xs.len() - 1;
 		}
 		else
 		{
-			loc += rem - 1;
+			println!("4loc {} rem {} len {}", loc, rem, xs.len());
+			loc = loc + rem;
+			loc = loc - 1;
 		}
 		vec.push(xs[loc]);
 		xs.remove(loc);
@@ -21,17 +33,30 @@ fn josephus_recursion<T:Clone+Copy>(xs:Vec<T>, k:usize, l:usize) -> Vec<T> {
 		{
 			if xs.len() == loc
 			{
+				println!("5loc {} rem {} len {}", loc, rem, xs.len());
 				loc = 0;
 			}
-			vec = josephus_recursion(xs, k, loc)
+			vec.extend(josephus_recursion(xs, k, loc));
 		}
 	}
+	// else if xs.len() == 1 
+	// {
+	// 	vec.push(xs[0]);
+	// 	xs.remove(loc);
+	// }
 	vec
 }
 
 fn josephus<T:Clone+Copy + std::fmt::Debug>(xs:Vec<T>,k:usize)-> Vec<T> {
     
-    return (josephus_recursion(xs, k, 0));
     println!("{:?}", xs);
-    xs
+	let v = josephus_recursion(xs, k, 0);
+	println!("{:?}", v);
+	v
+}
+
+fn main()
+{
+	josephus(vec![1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 1);
+	josephus("CodeWars".chars().collect::<Vec<char>>(), 4);
 }
