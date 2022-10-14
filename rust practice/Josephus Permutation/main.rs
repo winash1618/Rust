b36383d5919b59c3242beba2c67ab172eb624b2b
Muhammed Asharaf K V <51042907+winash1618@users.noreply.github.com>
2022-10-14 13:33:08 +0000

    final submitted clean version

--- rust practice/Josephus Permutation/main.rs
+++ rust practice/Josephus Permutation/main.rs
@@ -8,24 +8,19 @@ fn josephus_recursion<T:Clone+Copy + std::fmt::Debug>(xs:Vec<T>, k:usize, loc:us
 	{
 		if xs.len() < k
 		{
-			println!("1loc {} rem {} len {}", loc, rem, xs.len());
 			rem = k % xs.len();
 		}
 		if loc == 0 && rem == 0
 		{
-			println!("2loc {} rem {} len {}", loc, rem, xs.len());
 			loc = xs.len() - 1;
 		}
 		else if (loc + rem) > xs.len()
 		{
-			println!("3loc {} rem {} len {}", loc, rem, xs.len());
 			loc = (loc + rem) - xs.len() - 1;
 		}
 		else
 		{
-			println!("4loc {} rem {} len {}", loc, rem, xs.len());
-			loc = loc + rem;
-			loc = loc - 1;
+			loc = loc + rem - 1;
 		}
 		vec.push(xs[loc]);
 		xs.remove(loc);
@@ -33,30 +28,39 @@ fn josephus_recursion<T:Clone+Copy + std::fmt::Debug>(xs:Vec<T>, k:usize, loc:us
 		{
 			if xs.len() == loc
 			{
-				println!("5loc {} rem {} len {}", loc, rem, xs.len());
 				loc = 0;
 			}
 			vec.extend(josephus_recursion(xs, k, loc));
 		}
 	}
-	// else if xs.len() == 1 
-	// {
-	// 	vec.push(xs[0]);
-	// 	xs.remove(loc);
-	// }
 	vec
 }
 
 fn josephus<T:Clone+Copy + std::fmt::Debug>(xs:Vec<T>,k:usize)-> Vec<T> {
-    
-    println!("{:?}", xs);
 	let v = josephus_recursion(xs, k, 0);
-	println!("{:?}", v);
 	v
 }
 
-fn main()
-{
-	josephus(vec![1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 1);
-	josephus("CodeWars".chars().collect::<Vec<char>>(), 4);
-}
+/*************************************************************************/
+/*************************** Best Solution *******************************/
+/*************************************************************************/
+
+// fn josephus<T: std::fmt::Debug>(mut live: Vec<T>, k: usize) -> Vec<T> {
+//     let mut dead = Vec::new();
+//     let mut i = (k - 1) % live.len();
+//     loop {
+//         dead.push(live.remove(i));
+//         if live.is_empty() {
+//             break;
+//         }
+//         i = (i + k - 1) % live.len();
+//     }
+// 	// println!("{:?}", dead);
+//     dead
+// }
+
+// fn main()
+// {
+// 	josephus(vec![1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 1);
+// 	josephus("CodeWars".chars().collect::<Vec<char>>(), 4);
+// }
