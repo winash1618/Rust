d344151f1334dde66acb6895cc6008a444830cd4
Muhammed Asharaf K V <51042907+winash1618@users.noreply.github.com>
2022-10-15 04:59:04 +0000

    Second attempt not optimized

--- rust practice/Hamming Numbers/main.rs
+++ rust practice/Hamming Numbers/main.rs
@@ -1,36 +1,42 @@
-fn recursive_hammer(n: usize, i: u64, j: u64, k: u64, mut prod: u64, count: usize) -> u64
-{
-	let a : u64 = 2;
-	let b : u64 = 3;
-	let c : u64 = 5;
-	if count <= n
-	{
-		let ap : u64 = prod * a.pow(i as u32 + 1);
-		let bp : u64 = prod * b.pow(j as u32 + 1);
-		let cp : u64 = prod * c.pow(k as u32 + 1);
-		println!("{} {} {}", i, j, k);
-		if ap < bp && ap < cp
-		{
-			prod = recursive_hammer(n, i + 1, j, k, ap, count + 1);
-		}
-		else if bp < ap && bp < cp
-		{
-			prod = recursive_hammer(n, i, j + 1, k, bp, count + 1);
-		}
-		else
-		{
-			prod = recursive_hammer(n, i, j, k + 1, cp, count + 1);
-		}
-		println!("{} {}", prod, count);
-	}
-	prod
-}
+
 
 fn hamming(n: usize) -> u64 {
-    recursive_hammer(n, 0, 0, 0, 1, 1)
+    if n == 1
+	{
+		return 1;
+	}
+	let mut min_value : u64 = 0;
+	let mut vec = Vec::new();
+	let mut count = 1;
+	vec.push(1);
+	while count <= n
+	{
+		// println!("{:?}", vec);
+		min_value = *vec.iter().min().unwrap() as u64;
+		if vec.contains(&(min_value * 2)) == false
+		{
+			vec.push(min_value * 2);
+		}
+		if vec.contains(&(min_value * 3)) == false
+		{
+			vec.push(min_value * 3);
+		}
+		if vec.contains(&(min_value * 5)) == false
+		{
+			vec.push(min_value * 5);
+		}
+		vec.remove(vec
+			.iter()
+			.position(|&x| x == min_value)
+			.unwrap());
+		count += 1;
+		// if (conut + vec.len() > n)
+		// 	min_value = hammer(vec)
+	}
+	min_value
 }
 
 fn main () {
-	let ham = hamming(10);
+	let ham = hamming(5000);
 	println!("{}", ham);
 }
