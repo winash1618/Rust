df62e41fa991a454c78a99ba62c3855d5593b3ef
Muhammed Asharaf K V <51042907+winash1618@users.noreply.github.com>
2022-10-15 05:39:06 +0000

    final submitted version

--- rust practice/Hamming Numbers/main.rs
+++ rust practice/Hamming Numbers/main.rs
@@ -9,7 +9,6 @@ fn hamming(n: usize) -> u64 {
 	vec.push(1);
 	while count <= n
 	{
-		println!("{:?} {}", vec, count);
 		min_value = *vec.iter().min().unwrap() as u64;
 		let max = u64::MAX;
 		if (max / min_value) >= 2 && vec.contains(&(min_value * 2)) == false
@@ -29,21 +28,72 @@ fn hamming(n: usize) -> u64 {
 			.position(|&x| x == min_value)
 			.unwrap());
 		count += 1;
-		// if (conut + vec.len() > n)
-		// 	min_value = hammer(vec)
 	}
 	min_value
 }
 
 fn main () {
 	let ham = hamming(5000);
-	// let mut i : u64= 2;
-	// let mut count = 0;
-	// loop
-	// {
-	// 	i = i * 2 as u64;
-	// 	count += 1;
-	// 	println!("{}", count);
-	// }
 	println!("{}", ham);
 }
+
+/*************************************************************************/
+/*************************** Best Solutions *******************************/
+/*************************************************************************/
+
+// fn hamming(n: usize) -> u64 {
+//     let mut output:Vec<u64> = vec![0; n+1];
+//     output[1] = 1;
+//     let (mut p2, mut p3, mut p5) = (1,1,1);
+//     for i in 2..=n {
+//         let (num2, num3, num5) = (output[p2] * 2, output[p3] * 3, output[p5] * 5);
+//         output[i] = num2.min(num3).min(num5);
+//         if output[i] == num2 {
+//             p2 += 1;
+//         }  
+//         if output[i] == num3 {
+//             p3 += 1;
+//         }  
+//         if output[i] == num5 {
+//             p5 += 1;
+//         }  
+//     }
+//     output[n]
+// }
+
+// pub fn hamming(n: usize) -> u64 {
+// 	use std::collections::BTreeSet;
+
+// 	let mut s: BTreeSet<u64> = BTreeSet::from([1]);
+// 	for _ in 1..n {
+// 		let x = *s.iter().next().unwrap();
+// 		s.remove(&x);
+// 		s.insert(x.checked_mul(2).unwrap_or(u64::MAX));
+// 		s.insert(x.checked_mul(3).unwrap_or(u64::MAX));
+// 		s.insert(x.checked_mul(5).unwrap_or(u64::MAX));
+// 	}
+// 	*s.iter().next().unwrap()
+// }
+
+// use std::{collections::BTreeSet};
+
+// fn next_hamming(hamming: &mut BTreeSet<u64>, n: u64) {
+//     if !hamming.contains(&n) {
+//         hamming.insert(n);
+//         if n < u64::MAX / 2 {
+//             next_hamming(hamming, n * 2);
+//         }
+//         if n < u64::MAX / 3 {
+//             next_hamming(hamming, n * 3);
+//         }
+//         if n < u64::MAX / 5 {
+//             next_hamming(hamming, n * 5);
+//         }
+//     }
+// }
+
+// fn hamming(n: usize) -> u64 {
+//     let mut hamming: BTreeSet<u64> = BTreeSet::new();
+//     next_hamming(&mut hamming, 1);
+//     *hamming.iter().nth(n - 1).unwrap()
+// }
