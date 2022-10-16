e6c87558bc0b03e9ce622a7c21eb49297ce71266
Muhammed Asharaf K V <51042907+winash1618@users.noreply.github.com>
2022-10-16 10:54:04 +0000

    working submitted version

--- rust practice/Best travel/main.rs
+++ rust practice/Best travel/main.rs
@@ -1,32 +1,45 @@
 fn choose_best_sum(t: i32, k: i32, ls: &Vec<i32>) -> i32 {
-	if ls.len()  < k
-		return (-1);
-	let mut lst = ls;
-	max1 = *lst.iter().min().unwrap();
-	lst.remove(lst
-		.iter()
-		.position(|&x| x == max1)
-		.unwrap());
-	max2 = *lst.iter().min().unwrap();
-	lst.remove(lst
-		.iter()
-		.position(|&x| x == max2)
-		.unwrap());
-	let mut tmp = 0;
-	for i in &lst {
-		// iterate immutably
-		let i: &i32 = i; // elements are immutable pointers
-		if t - tmp > t - max1 - max2 - i
+	let lst = ls;
+	let mut i = 0;
+	let mut d = 0;
+	while (i + k - 1) < lst.len() as i32
+	{
+		println!("{:?} {} {} {}", lst, d, k, t);
+		let mut dis = ls[i as usize];
+		if k > 1
 		{
-			tmp = max1 + max2 + i;
+			let save_one = lst[i as usize];
+			dis = choose_best_sum(t - save_one, k - 1, &(lst[((i + 1) as usize)..]).to_vec());
+			println!("{} {}", dis, save_one);
+			if dis > 0
+			{
+				dis = save_one + dis;
+			}
+			else
+			{
+				dis = -1;
+			}
 		}
+		if t - dis < t - d && t - dis >= 0
+		{
+			d = dis;
+		}
+		i += 1;
 	}
-	if tmp != 0 && tmp > max1 + max2 + i
+	if d <= 0
 	{
 		return -1;
 	}
 	else
 	{
-		
+		return d;
 	}
+}
+
+fn main()
+{
+	// let ts = &vec![50, 55, 56, 57, 58];
+    let ts = &vec![91, 74, 73, 85, 73, 81, 87];
+	let p = choose_best_sum(331, 2, ts);
+	println!("{}", p);
 }
